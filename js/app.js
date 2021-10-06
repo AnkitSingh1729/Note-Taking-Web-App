2926a1443cd9afdf2c2dd434782937985fcda3d8
AnkitSingh1729 <64279281+AnkitSingh1729@users.noreply.github.com>
2021-10-06 14:58:09 +0530

    Post new user data to server

--- js/app.js
+++ js/app.js
@@ -1,69 +1,93 @@
 console.log("Welcome");
 
-// if user adds a note, add it to the local storage
-
+// if user adds a note, add it to the server(crudecrude for now)
 let addBtn = document.getElementById('addBtn');
 showNotes();                                                      // Required to update the notes when website is opened for the first time.
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
-    let notes = localStorage.getItem("notes");
-    if (notes == null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    }
-    notesObj.push(addTxt.value);                                    // Push (.value) of "addTxt" to the array we just created
-    localStorage.setItem("notes", JSON.stringify(notesObj));        // Set the array we created(nodeObj) back to localStorage
+    let addTxtValue = addTxt.value;
+
+    const obj = { // Create object to be posted through axios
+        addTxtValue,
+    };
+
+    axios    // Post data from textbox to server
+        .post('https://crudcrud.com/api/769f08c7f18e46289d1e15987c8e9b60/notesData', obj)
+        .then(res => {
+            console.log("Data has been posted to crudcrud");
+            console.log(res);
+        })
+        .catch(err => {
+            console.log("Error Message: ");
+            console.log(res);
+        })
+
     addTxt.value = "";                                              // It will remove the text form addTxt area after the addBtn is clicked
-    console.log(notesObj);
     showNotes();
 })
 
 
-// This function shows the current localStorage data on the website where it's asked to show it.
-// i.e. it updates the innerHTML of a specific element based on current localStorage.
 function showNotes() {
-    notes = localStorage.getItem("notes");
-    if (notes == null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    }
-    let html = "";
-    notesObj.forEach(function (element, index) {                                                           // Entry coming from localStorage
-        // Inplace of this.id, we can also use delete(${index}) directly
-        html += `                                                                                   
-        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-            
-            <div class="card-body">
-                <h5 class="card-title">Note ${index + 1}</h5>
-                <p class="card-text">${element}</p>
-                <button id=${index} onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>        
-            </div>
-        </div>`;
-    });
+    axios  
+        .get('https://crudcrud.com/api/769f08c7f18e46289d1e15987c8e9b60/notesData')
+        .then(res => {
+            console.log(res);
+        
+            if (notes == null) {
+                notesArr = [];
+            } else {
+                notesArr = res.data;
+            }
 
-    let notesElm = document.getElementById("notes");                                                        // Pick element with id="notes", we will change its innerHTMl here
-    if (notesObj.length != 0) {
-        notesElm.innerHTML = html
-    } else {
-        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
-    }
+
+            let html = "";
+            notesArr.forEach(function (element, index) {                                                           // Entry coming from localStorage
+                // Inplace of this.id, we can also use delete(${index}) directly
+                console.log(element._id);
+                html += `                                                                                   
+                <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text">${element.addTxtValue}</p>
+                        <button id=${index} onclick="deleteNote(${element._id})" class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`;
+            });
+
+            let notesElm = document.getElementById("notes");                                                        // Pick element with id="notes", we will change its innerHTMl here
+            if (notesArr.length != 0) {
+                notesElm.innerHTML = html
+            } else {
+                notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+            }
+
+
+        })
+        .catch(err => {
+            console.log("Error occured while getting data from server")
+            console.log(err);
+        })
+   
 }
 
-function deleteNote(index){
-    console.log('I am deleting', index);
-    let notes = localStorage.getItem("notes");
-    if (notes == null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    }
-
-    notesObj.splice(index, 1);                                                                              // Delete 1 element at specific index
-    localStorage.setItem("notes", JSON.stringify(notesObj));                                                // then set the value of updated array in localStorage after stringify it
+function deleteNote(idx){
+    console.log('I am deleting', idx);
+ 
+    axios  
+        .delete(`https://crudcrud.com/api/769f08c7f18e46289d1e15987c8e9b60/notesData/${idx}`)
+        .then(res => {
+            console.log(`Element with id: ${idx}, deleted`);
+            showNotes();
+        })
+        .catch(err => {
+            console.log("Error occured while deleting data from server")
+            console.log(err);
+        })
     showNotes();                                                                                            // call showNotes() so that the change can reflect on the website  
 }
+
+
 
 let search = document.getElementById("searchTxt");
 search.addEventListener("input", function(){
